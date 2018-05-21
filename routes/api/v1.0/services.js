c8cc8833fe6abefa36ce1c86cda184a6dbca7349
Gonzalo espinpza <gonis1990@gmail.com>
2018-05-20 22:55:09 -0400

    registro de usuarios en mogoDB

--- routes/api/v1.0/services.js
+++ routes/api/v1.0/services.js
@@ -1,12 +1,36 @@
 'use strict'
 
+const mogoose =require('mongoose')
+const connect =  require('../../../database/collections/connect')
+const Registro = require('../../../database/collections/users')
 const express = require('express')
+
+
 const route = express.Router()
+
+
+// metodos de peticion GET, POTS, PUT, DELETE
 
 route.get('/p1', (req, res) =>{
     res.send({ menssage:'mensaja de prueba'})
 })
 
+route.post('/registro', (req, res) =>{
+    console.log('POST /api/registro')
+    console.log(req.body)
 
+    let registro = new Registro()
+    registro.name =req.body.name
+    registro.lastname = req.body.lastname
+    registro.email = req.body.email
+    registro.password = req.body.password
+    registro.imguser = req.body.imguser
+
+    registro.save((err, usertStored) =>{
+        if(err) res.status(500).send({messaje: `Error al savar la base de datos:${err}`})
+  
+        res.status(200).send({usertStored})
+    })         
+})
 
 module.exports = route
